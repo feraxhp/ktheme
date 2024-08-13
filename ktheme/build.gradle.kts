3ee380d65f2816757bb7f2e6511471960db3fbdb
feraxhp <feraxhp+gh@gmail.com>
2024-08-13 18:18:35 -0500

    # Version 0.0.2:
    Ios binaries added.

--- ktheme/build.gradle.kts
+++ ktheme/build.gradle.kts
@@ -145,7 +145,7 @@ mavenPublishing {
     coordinates(
         groupId = "com.feraxhp.ktheme",
         artifactId = "ktheme-compose",
-        version = "0.0.1"
+        version = "0.0.2"
     )
 
     // Configure POM metadata for the published artifact
