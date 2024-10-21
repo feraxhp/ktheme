6334c8d3eb6691fe31a12c11bb15e843d64733bd
feraxhp <feraxhp+gh@gmail.com>
2024-10-20 20:03:13 -0500

    * SettingsScreen

--- sample/composeApp/src/commonMain/kotlin/com/feraxhp/sample/screens/SettingsScreen.kt
+++ sample/composeApp/src/commonMain/kotlin/com/feraxhp/sample/screens/SettingsScreen.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
@@ -40,6 +42,7 @@ class SettingsScreen : Screen {
                     Row(
                         Modifier
                             .padding(top = 24.dp)
+                            .widthIn(Dp.Unspecified, 450.dp)
                             .align(Alignment.CenterHorizontally)
                         ,
                         horizontalArrangement = Arrangement.Center,
